8ca8a723fcab725842fe4196a0b4c78965d93291
Tobias T <t.terhoerst@gmx.de>
2024-08-07 23:47:44 +0200

    Add new swagger ui documentation to the endpoint

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
---
+++ installations/install_sqlsrv_extensions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+phpVersion=$1
+if [ -z "${phpVersion}" ]; then
+  echo "Could not find php version, please introduce your current php version"
+  exit 1
+fi
+
+apt install php-pear -y
+
+pecl install sqlsrv
+pecl install pdo_sqlsrv
+
+printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/$phpVersion/mods-available/sqlsrv.ini
+printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/$phpVersion/mods-available/pdo_sqlsrv.ini
+
+phpenmod -v $phpVersion sqlsrv pdo_sqlsrv
